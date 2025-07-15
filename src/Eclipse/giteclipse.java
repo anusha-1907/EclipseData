a8fdc98581a399d0a52adc8fbd5e6adb8b009045
anusha-1907 <anushasagadabona@gmail.com>
2025-07-15 16:58:02 +0530

    edit in eclipse

---
+++ src/Eclipse/giteclipse.java
@@ -0,0 +1,10 @@
+package Eclipse;
+
+public class giteclipse {
+
+	public static void main(String[] args) {
+    System.out.println("username");
+    System.out.println("password");
+	}
+
+}
---
+++ src/Eclipse/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Anusha
+ *
+ */
+package Eclipse;
